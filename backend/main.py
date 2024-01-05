fd8c34e28e48a473603132b7decdeed12f8475c6
ikeda042 <ikeda042main@gmail.com>
2024-01-05 17:42:55 +0000

    init setup

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,7 @@ import numpy as np
 import shutil
 import os 
 import uvicorn
+from datetime import datetime
 
 
 app = FastAPI(
@@ -16,9 +17,19 @@ app = FastAPI(
     openapi_url="/api/v2/openapi.json"
 )
 
+# CORSミドルウェアの設定
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Reactアプリのオリジンを許可
+    allow_credentials=True,
+    allow_methods=["*"],  # すべてのHTTPメソッドを許可
+    allow_headers=["*"],  # すべてのヘッダーを許可
+)
+
 @app.get("/api/v2/hello-world")
 async def hello():
-    return {"message": "Hello World"}
+    print(datetime.now())
+    return {"message": "Hello baWorld"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000, log_level="info",host="0.0.0.0")
+    uvicorn.run("main:app", port=8000, log_level="info",host="0.0.0.0", reload=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/v2/hello-world')
+      .then(response => response.json()) // レスポンスをJSONとして解析
+      .then(data => setMessage(data.message)) // JSONオブジェクトからmessageプロパティの値を取得
+      .catch(err => console.error('Error fetching data:', err));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. {message}
         </p>
         <a
           className="App-link"
